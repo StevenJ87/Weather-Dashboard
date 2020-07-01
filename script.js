b5861d5c8510a0e614f53b5483c1170c0dd2fab1
Steven Johnson <sjohnson21960@gmail.com>
2020-06-30 22:36:30 -0500

    Creating buttons and local storage

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+var cities = []
+
+$("document").ready(function(){
+    buttons();
+});
+
+function buttons(){
+    for(var i=0;i<cities.length;i++){
+    $(".cityList").append("<li>").append($("<button>").text(cities[i]));
+}};
+
 $(".btn-primary").click(function(){
-    $(".cityList").append("<li>").append($("<button>").html($("searchName").text()))
+
+    $(".cityList").empty();
+    var APIKey = "4759b868e6e8cc03f5fdb881f043cd77";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + $(".searchName").val() + "&appid=" + APIKey;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+
+        console.log(response)
+
+        cities.push(response.name)
+
+        localStorage.setItem(response.name, JSON.stringify(response));
+
+        buttons();
+    });
 });
